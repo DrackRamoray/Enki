7651cfb46569f5b8da0708b0ef684ad14e429ba5
DrackRamoray <DrackRamoray@gmail.com>
2023-03-23 13:08:17 +0800

    feat: add feature

--- src-shared/src/convert-src.rs
+++ src-shared/src/convert-src.rs
@@ -1,5 +1,17 @@
 use urlencoding::encode;
 
+#[cfg(feature = "win")]
+pub fn convert_src(src: &str) -> String {
+    if src.starts_with("https://asset.localhost/") {
+        src.to_string()
+    } else {
+        let url = encode(src);
+
+        format!("https://asset.localhost/{}", url)
+    }
+}
+
+#[cfg(not(feature = "win"))]
 pub fn convert_src(src: &str) -> String {
     if src.starts_with("asset://localhost/") {
         src.to_string()
